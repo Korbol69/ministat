24feff1cf4cd54e95b352ca2e29c58239df695dd
Jay Grizzard <elfchief-smartgit@lupine.org>
2013-07-31 10:26:59 -0700

    remove default comments so 'brew audit' will be happy

--- homebrew/ministat.rb
+++ homebrew/ministat.rb
@@ -6,16 +6,10 @@ class Ministat < Formula
   sha1 '0f21711413d294952a44b50c40693f1a178c79a5'
   version '1.0.20130731'
 
-  # depends_on 'cmake' => :build
-  # depends_on :x11 # if your formula requires any X11/XQuartz components
-
   def install
-    # ENV.j1  # if your formula's build system can't parallelize
-
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
